290c21886af0d054b413a3ac9d7b4d3ea00eef56
Timothy <timy.djon@gmail.com>
2024-06-10 14:02:15 +0200

    ignore @appply warnings

--- lua/timy/core/options.lua
+++ lua/timy/core/options.lua
@@ -10,6 +10,7 @@ opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.updatetime = 100
 
 -- line wrapping
 opt.wrap = true -- disable line wrapping
